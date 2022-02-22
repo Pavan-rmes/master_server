f802e1061dcebfa6ee40060a9aa95b1285bf3a3e
pavan <pavanguntupally12@gmail.com>
2022-02-22 10:43:12 +0530

    master server

--- src/realtime.js
+++ src/realtime.js
@@ -1,68 +1,73 @@
 import { Router } from "express";
-import { SocketActivation,SocketDeactivate,changeDgaValues,ChangeValues,GetValues,getPresentPort,ChangeAmbTemp,changeNameplate,GetNameplateValues,getFanbankStatus,ChangeFanbankStatus } from "./index.js";
+// import { SocketActivation,SocketDeactivate,changeDgaValues,ChangeValues,GetValues,getPresentPort,ChangeAmbTemp,changeNameplate,GetNameplateValues,getFanbankStatus,ChangeFanbankStatus } from "./index.js";
 import express from "express";
+import axios from "axios";
+
+
+const trafoApi = "http://127.0.0.1"
 
 const realTimeRouter = Router();
 
 realTimeRouter.post("/",express.json(),(req,res)=>{
-    const filter = req.body
-    console.log(filter)
-    const {regulation,port,automatic,percentage} = filter
-    let load = percentage
-    res.send(GetValues())
-    if(getPresentPort() === +port) {
-        'pass'
-    }
-    else{
-        SocketDeactivate()
-        SocketActivation(+port)
-    }
-    ChangeValues(regulation,automatic,load)
+    const {id} = req.query
+    axios.post(`${trafoApi}:${9000+(+id)}/trafo`,req.body)
+    .then(()=>res.send({status:"success"}))
     
 })
 
 realTimeRouter.get("/",(req,res)=>{
-    res.send(GetValues())
+    const {id} = req.query
+    axios.get(`${trafoApi}:${9000+(+id)}/trafo`)
+    .then((data) => res.send(data.data))
+    .catch((err)=>res.send(err))
+
 })
 
+
 realTimeRouter.post("/dga",express.json(),(req,res)=>{
-    changeDgaValues(req.body)
-    console.log(req.body)
-    res.send({status:"success"})
+    const {id} = req.query
+    axios.post(`${trafoApi}:${9000+(+id)}/trafo/dga`,req.body)
+    .then(()=>res.send({status:"success"}))
     
 })
 
-realTimeRouter.get("/nameplate",(req,res)=>{
-    res.send(GetNameplateValues())
+realTimeRouter.get("/nameplate",async(req,res)=>{
+    const {id} = req.query
+    axios.get(`${trafoApi}:${9000+(+id)}/trafo/nameplate`)
+    .then((data) => res.send(data.data))
+    .catch((err)=>res.send(err))
+
 })
 
-realTimeRouter.post("/nameplate",express.json(),(req,res)=>{
-    changeNameplate(req.body)
-    res.send({stats:"success"})
+realTimeRouter.post("/nameplate",express.json(),async(req,res)=>{
+    const {id} = req.query
+    const rating = req.body
+    axios.post(`${trafoApi}:${9000+(+id)}/trafo/nameplate`, rating)
+    .then((data)=>res.send({stats:"success"}))
+    // changeNameplate(req.body)
 })
 
 realTimeRouter.post("/ambtemp",express.json(),(req,res)=>{
-    ChangeAmbTemp(req.body.ambTemp)
-    res.send({status:"success"})
+    const {id} = req.query
+    axios.post(`${trafoApi}:${9000+(+id)}/trafo/ambtemp`,req.body)
+    .then(()=>res.send({status:"success"}))
 })
 
 realTimeRouter.get("/fanbank",(req,res)=>{
-    res.send(getFanbankStatus())
+    const {id} = req.query
+    axios.get(`${trafoApi}:${9000+(+id)}/trafo/fanbank`)
+    .then((data) => res.send(data.data))
+    .catch((err)=>res.send(err))
 })
 
 realTimeRouter.post("/fanbank",express.json(),(req,res)=>{
-    ChangeFanbankStatus(req.body)
-    res.send({status:"success"})
+    const {id} = req.query
+    axios.post(`${trafoApi}:${9000+(+id)}/trafo/fanbank`,req.body)
+    .then(()=>res.send({status:"success"}))
+    .catch((err)=>res.send(err))
+
 })
 
-realTimeRouter.get("/trafoname",(req,res)=>{
-    res.send(getTrafoName())
-})
-
-realTimeRouter.post("/trafoname",(req,res)=>{
-    ChangeTrafoName(req.body)
-    res.send({status:"Success"})
-})
 
 
 export default realTimeRouter;
